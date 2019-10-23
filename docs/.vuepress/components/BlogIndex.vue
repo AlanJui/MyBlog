217a33728b8f71a0595ff27d1c9a99cdce994dea
居正中 <AlanJui.1960@gmail.com>
2019-10-23 12:27:55 +0800

    在「文章清單」，對每篇文章標示「發行日期」、「類別標籤」

--- docs/.vuepress/components/BlogIndex.vue
+++ docs/.vuepress/components/BlogIndex.vue
@@ -5,8 +5,16 @@
         <router-link :to="post.path">{{ post.frontmatter.title }}</router-link>
       </h2>
       <div class="tags">
-        <span class="publish-date">{{formateDate(post.frontmatter.date)}}</span>
-        <span class="tag" v-for="tag in post.frontmatter.tags">{{tag}}</span>
+        <span>
+          <Fa-CalendarAlt color="green" size="2x" />
+        </span>
+        <span class="publish-date">
+          {{formateDate(post.frontmatter.date)}}
+        </span>
+        &nbsp;&nbsp;<Fa-FileCode color="green" size="2x" />&nbsp;&nbsp;
+        <span class="tag" v-for="tag in post.frontmatter.tags">
+          {{tag}}
+        </span>
       </div>
 
       <p>{{ post.frontmatter.description }}</p>
@@ -44,23 +52,29 @@
     padding: 20px;
   }
 
+  span {
+    display: inline-block;
+    vertical-align: middle;
+    line-height: normal;
+  }
+
   .tags {
-    margin-top: 10px;
+    /*margin-top: 5px;*/
   }
 
   .tag {
     padding: 5px;
     border-radius: 7px;
     font-size: small;
-    background: #3eaf7c;
+    background: steelblue;
     margin-right: 5px;
     color: white;
     font-weight: 500;
   }
 
   .publish-date {
-    color: #3eaf7c;
-    overflow: hidden;
-    text-overflow: ellipsis;
+    padding-top: 5px;
+    color: steelblue;
+    font-weight: bold;
   }
 </style>
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,5 +20,19 @@ module.exports = {
     anchor: { permalink: false },
     // options for markdown-it-toc
     toc: { includeLevel: [1, 4] }
+  },
+  thirdPartyComponents: {
+    fontAwesomeIcons: {
+      // Regular font awesome icon keys here
+      regular: [
+        'address-card',
+        'file-audio',
+        'cog',
+        'calendar-alt',
+        'file-code'
+      ],
+      // Solid font awesome icon keys here
+      solid:['battery-quarter']
+    }
   }
 }
